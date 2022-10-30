53e377295c34a8dbf2210a2e06a06a87f6bd06f1
Masum-Arifin <masumarifinbd@gmail.com>
2022-10-30 19:20:02 +0600

    Filter youtubing  adedd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import Filter from './Filter';
 import Header from './Header';
 import Sidebar from './Sidebar';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
      <Header/>
      <Sidebar/>
+     <Filter/>
     </div>
   );
 }
--- src/Filter.js
+++ src/Filter.js
@@ -3,7 +3,9 @@ import './Filter.css'
 const Filter = () => {
     return (
         <div className="filter d-flex align-items-center">
-            
+            <div className="single-filter">
+        <span className="filter-items pointer active">All</span>
+      </div>
         </div>
     );
 };
