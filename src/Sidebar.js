5194959c0cac7827fc1dedb0a5b27905a9a5d7ce
Masum-Arifin <masumarifinbd@gmail.com>
2022-10-30 18:58:16 +0600

    music svg adedd

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -75,6 +75,24 @@ const Sidebar = () => {
         <span className="sidebar-text">Subscriptions</span>
       </div>
 
+      <div className="sidebar-items d-flex align-items-center active">
+        <svg
+          viewBox="0 0 24 25"
+          preserveAspectRatio="xMidYMid meet"
+          focusable="false"
+          class="style-scope yt-icon"
+          style={{ width: "20px", height: "20px", margin: "0px 20px" }}
+        >
+          <g class="style-scope yt-icon">
+            <path
+              d="M10 9.35L15 12l-5 2.65zM12 7a5 5 0 105 5 5 5 0 00-5-5m0-1a6 6 0 11-6 6 6 6 0 016-6zm0-3a9 9 0 109 9 9 9 0 00-9-9m0-1A10 10 0 112 12 10 10 0 0112 2z"
+              class="style-scope yt-icon"
+            ></path>
+          </g>
+        </svg>
+        <span className="sidebar-text">Youtube Music</span>
+      </div>
+<p></p>
         </div>
     );
 };
