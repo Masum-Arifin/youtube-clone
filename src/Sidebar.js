513781440e062e7b6247e26daeb5def237ba738b
Masum-Arifin <masumarifinbd@gmail.com>
2022-10-30 19:05:29 +0600

    Downloads svg adedd

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -164,6 +164,26 @@ const Sidebar = () => {
         </svg>
         <span className="sidebar-text">Watch later</span>
       </div>
+
+      <div className="sidebar-items d-flex align-items-center active">
+        <svg
+          viewBox="0 0 24 25"
+          preserveAspectRatio="xMidYMid meet"
+          focusable="false"
+          class="style-scope yt-icon"
+          style={{ width: "20px", height: "20px", margin: "0px 20px" }}
+        >
+          <g class="style-scope yt-icon">
+            <path
+              d="M17 18V19H6V18H17ZM16.5 11.4L15.8 10.7L12 14.4V4H11V14.4L7.2 10.6L6.5 11.3L11.5 16.3L16.5 11.4Z"
+              class="style-scope yt-icon"
+            ></path>
+          </g>
+        </svg>
+        <span className="sidebar-text">Downloads</span>
+      </div>
+
+      
         </div>
     );
 };
