9d45dd2288891f79bbfee8c7789a032ed1dec00a
Masum-Arifin <masumarifinbd@gmail.com>
2022-10-30 18:47:16 +0600

    sidebar css adedd

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -57,6 +57,24 @@ const Sidebar = () => {
         <span className="sidebar-text">Shorts</span>
       </div>
       
+      <div className="sidebar-items d-flex align-items-center active">
+        <svg
+          viewBox="0 0 24 25"
+          preserveAspectRatio="xMidYMid meet"
+          focusable="false"
+          class="style-scope yt-icon"
+          style={{ width: "20px", height: "20px", margin: "0px 20px" }}
+        >
+          <g class="style-scope yt-icon">
+            <path
+              d="M10,18v-6l5,3L10,18z M17,3H7v1h10V3z M20,6H4v1h16V6z M22,9H2v12h20V9z M3,10h18v10H3V10z"
+              class="style-scope yt-icon"
+            ></path>
+          </g>
+        </svg>
+        <span className="sidebar-text">Subscriptions</span>
+      </div>
+
         </div>
     );
 };
