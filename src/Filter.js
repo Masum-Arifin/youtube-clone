e6931ec3d786bb576c812ec9b0a47e64b705a264
Masum-Arifin <masumarifinbd@gmail.com>
2022-10-30 19:35:59 +0600

    Filter youtubing  adedd

--- src/Filter.js
+++ src/Filter.js
@@ -6,6 +6,30 @@ const Filter = () => {
             <div className="single-filter">
         <span className="filter-items pointer active">All</span>
       </div>
+      <div className="single-filter">
+        <span className="filter-items pointer">Mixes</span>
+      </div>
+      <div className="single-filter">
+        <span className="filter-items pointer">Music</span>
+      </div>
+      <div className="single-filter">
+        <span className="filter-items pointer">live</span>
+      </div>
+      <div className="single-filter">
+        <span className="filter-items pointer">Simplilearn</span>
+      </div>
+      <div className="single-filter">
+        <span className="filter-items pointer">computer</span>
+      </div>
+      <div className="single-filter">
+        <span className="filter-items pointer">Programming</span>
+      </div>
+      <div className="single-filter">
+        <span className="filter-items pointer">ReactJS</span>
+      </div>
+      <div className="single-filter">
+        <span className="filter-items pointer">VueJS</span>
+      </div>
         </div>
     );
 };
